929345c93bbcf2653fad5538b33fbee8946934d0
Viktor Bogutskii <info@siteograf.com>
2021-04-12 16:00:04 -0400

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const bodyParser = require('./bodyParser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 bodyParser(app);
 routes(app);
